5a57d9952ddeb8ef3b9faea62e9889296f1d9ca7
ensembleVisGuidelines <61663479+ensembleVisGuidelines@users.noreply.github.com>
2021-03-18 22:44:16 -0400

    Add files via upload

---
+++ data/authorized_tags.js
@@ -0,0 +1,70 @@
+define({
+	authorizedTags: {
+		"localization:extreme value": {
+			"description": "to locate the item(s) with maximum or the minimum value"
+		},
+		"localization:extreme change of values": {
+			"description": "to locate the region(s) with highest/lowest increase/decrease"
+		},
+		"localization:extreme average of values": {
+			"description": "to locate the region(s) with maximal or minimal average value"
+		},
+		"localization:outlier": {
+			"description": "to locate the item(s) whose attributes are different than the majority's"
+		},
+		"localization:pattern": {
+			"description": "to locate the region(s) with certain appearance or behavior of interest"
+		},
+		"localization:other specific attributes": {
+			"description": "to locate the item(s) with a specific attribute other than extreme, outlier, or pattern"
+		},
+		"identification:average": {
+			"description": "to identify the mean value of an attribute of a group of items"
+		},
+		"identification:extreme value": {
+			"description": "to identify the extreme value of an attribute of a group of items"
+		},
+		"identification:distribution spread": {
+			"description": "to identify the range or properties related to range of the distribution of a group of items"
+		},
+		"identification:pattern": {
+			"description": "to identify the type of the appearance or behavior of a group of items"
+		},
+		"identification:trend": {
+			"description": "to identify the qualitative relationship between two variables"
+		},
+		"identification:cluster": {
+			"description": "to seperate items into groups"
+		},
+		"identification:membership": {
+			"description": "to identify inclusion relation of item(s) and group(s)"
+		},
+		"comparison:numerosity": {
+			"description": "to compare the numbers of items in several groups"
+		},
+		"comparison:average": {
+			"description": "to compare the average values of several groups"
+		},
+		"comparison:extreme value": {
+			"description": "to compare the extrene values of several groups"
+		},
+		"comparison:change rate": {
+			"description": "to compare the change rates of several groups"
+		},
+		"comparison:spatial relationship": {
+			"description": "to compare the spatial properties such as magnitudes"
+		},
+		"comparison:distribution": {
+			"description": "to find the relationship rules which will predict behavior of one variable based on behavior of other variables"
+		},
+		"numeration:overall numerosity": {
+			"description": "to estimate the number of all items meeting certain condition in the whole range of visualization"
+		},
+		"numeration:extreme value of numerosity": {
+			"description": "to find the group with the largest/smallest number of items"
+		},
+		"numeration:numerosity in other specific range": {
+			"description": "to estimate the number of items in a given range"
+		}
+	}
+});
---
+++ data/tag_categories.js
@@ -0,0 +1,19 @@
+define({
+	tagCategories: {
+		"localization": {
+			"description": "to locate the positions of specific regions of interest in the ensembles"
+		},
+		"identification": {
+			"description": "to identify aggregate, statistical or topological properties"
+		},
+		"comparison": {
+			"desciption": "to compare the properties of multiple ensembles pairwise"
+		},
+		"association": {
+			"description": "to find the correspondences of multiple ensemble data sets"
+		},
+		"numeration": {
+			"description": "to estimate the number of items by subitizing or counting"
+		}
+	}
+});
